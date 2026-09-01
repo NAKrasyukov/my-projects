14c1ab7fd661f764c82ad07a87f2363ef315d654
NAKrasyukov <123346987+NAKrasyukov@users.noreply.github.com>
2026-09-01 10:47:51 +0500

    Add files via upload

---
+++ DBA/Sсripts/Умный шринк базы2.sql
@@ -0,0 +1,66 @@
+
+DECLARE @SQL NVARCHAR(MAX) = N'';
+
+SELECT @SQL = @SQL + N'
+USE ' + QUOTENAME(name) + N';
+
+DECLARE 
+    @FileName sysname,
+    @TargetSize INT,
+    @Factor FLOAT = .99,
+    @MinSize INT;
+
+-- Обрабатываем каждый DATA-файл базы
+DECLARE FileCursor CURSOR LOCAL FAST_FORWARD FOR
+SELECT 
+    name,
+    CEILING(FILEPROPERTY(name, ''SpaceUsed'') * 8.0 / 1024.0) + 1
+FROM sys.database_files
+WHERE type_desc = ''ROWS'';
+
+OPEN FileCursor;
+
+FETCH NEXT FROM FileCursor INTO @FileName, @MinSize;
+
+WHILE @@FETCH_STATUS = 0
+BEGIN
+    -- Определяем текущий размер файла и рассчитываем начальный TargetSize
+    SET @TargetSize = (
+        SELECT 1 + size * 8.0 / 1024 - 10000
+        FROM sys.database_files
+        WHERE name = @FileName
+    );
+
+    SET @TargetSize *= @Factor;
+
+    SELECT 
+        DB_NAME() AS DatabaseName,
+        @FileName AS FileName,
+        @TargetSize AS InitialTargetSize,
+        @MinSize AS MinSize;
+
+    WHILE @TargetSize > @MinSize
+    BEGIN
+        SET @TargetSize *= @Factor;
+
+        DBCC SHRINKFILE(@FileName, @TargetSize);
+
+        DECLARE @msg VARCHAR(200) = CONCAT(''Shrink file completed. Target Size: '', 
+             @TargetSize, '' MB. Timestamp: '', CURRENT_TIMESTAMP);
+        RAISERROR(@msg, 1, 1) WITH NOWAIT;
+
+        WAITFOR DELAY ''00:00:01'';
+    END;
+
+    FETCH NEXT FROM FileCursor INTO @FileName, @MinSize;
+END;
+
+CLOSE FileCursor;
+DEALLOCATE FileCursor;
+'
+FROM sys.databases
+WHERE database_id > 4
+  AND state_desc = 'ONLINE';
+
+  select @SQL
+EXEC sys.sp_executesql @SQL;
